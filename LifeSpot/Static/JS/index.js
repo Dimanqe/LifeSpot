0b5500241952b7d807f692c8ea1ade5d2f266ec6
Дмитрий Санкеев <d.sankeev@gmail.com>
2024-01-13 16:03:32 +0300

    updated!

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,8 +1,44 @@
-﻿
-alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+﻿let session = new Map();
+let sessionLog = function logSession() {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
+function handleSession() {
+
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+
+    // Запросим возраст пользователя и тоже сохраним
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    // Теперь мы возвращаем объект сессии
+    return session;
+}
+function CheckAge() {
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+
+// Вывод в консоль
+for (let result of session) {
+    console.log(result)
+};
+
 function filterContent() {
     // Сохраняем текст пользовательского запроса.
-    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+    // let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
     // Находим контейнеры с видео, которые необходимо фильтровать
     let elements = document.getElementsByClassName('video-container');
     // Пробегаемся по контейнерам
@@ -10,11 +46,43 @@ function filterContent() {
         // Вытаскиваем текст описания видео, которое необходимо отфильтровать
         let videoText = elements[i].querySelector(".video-title").innerText;
         // Выполняем фильтрацию, сравнивая значения в нижнем регистре
-        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+        if (!videoText.toLowerCase().includes(inputParseFunction.toLowerCase())) {
             // Описание
             elements[i].style.display = 'none';
         } else {
             elements[i].style.display = 'inline-block';
         }
     }
+};
+function ShowSubscriptionSuggestion() {
+    alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!")
+};
+const showSuggestion = setTimeout(ShowSubscriptionSuggestion, 30000);
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+}
+const writeReview = review => {
+    document.getElementsByClassName('review-container')[0].innerHTML += '<div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
 }
